27bd4135171874dfac77da38a740981bbc81649c
Raphael de Oliveira <raphaeldeobatista@gmail.com>
2026-01-26 22:42:27 -0300

    criando um banco de dados

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from sqlalchemy import sqlalchemy
+from flask_migrate import flask_migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = sqlalchemy(app)
+migrate = migrate(app,db)
 
 from estudo.views import homepage
+from estudo.models import Contato
---
+++ estudo/models.py
@@ -0,0 +1,10 @@
+from estudo import db
+from datetime import datetime
+
+class Contato(db.model):
+    id = db.column(db.Integer, primary_key = True)
+    data_envio = db.Column(db.DateTime, default=datetime.utcnow())
+    nome = db.Column(db.String, nullable=True)
+    email = db.Column(db.String, nullable=True)
+    assunto = db.Column(db.String, nullable=True)
+    mensagem = db.Column(db.String, nullable=True)
