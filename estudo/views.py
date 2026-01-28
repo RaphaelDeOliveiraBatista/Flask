8dd65a7119fc928a2bc54eb6e304e9a42130b7ed
Raphael de Oliveira <raphaeldeobatista@gmail.com>
2026-01-28 00:39:19 -0300

    criando um formlario GET e  POST para alimentar o banco de dados

--- estudo/views.py
+++ estudo/views.py
@@ -1,5 +1,6 @@
-from estudo import app
-from flask import render_template, url_for
+from estudo import app, db
+from flask import render_template, url_for, request
+from estudo.models import Contato
 
 @app.route('/')
 def homepage():
@@ -12,6 +13,29 @@ def homepage():
     }
     return render_template('index.html', context = context )
 
-@app.route('/contato/')
+@app.route('/contato/', methods=['GET', 'POST'])
 def novapag():
-    return 'outra view '
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        print('GET:',pesquisa)
+        context.update({'pesquisa': pesquisa})
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+        
+        contato = Contato(
+            nome=nome,
+            email = email,
+            assunto=assunto,
+            mensagem=mensagem,
+            
+        )
+
+        db.session.add(contato)
+        db.session.commit()
+
+
+    return render_template('contato.html', context=context)
